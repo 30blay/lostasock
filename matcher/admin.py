5f7ad8ae9a4aa765408252b825be54099caebf22
Unknown <tremblay.eti@gmail.com>
2019-10-12 18:21:21 -0400

    Add images to admin page

--- matcher/admin.py
+++ matcher/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Sock, Match
 
 admin.site.site_header = 'Lostasock Administration'
@@ -8,14 +9,34 @@ admin.site.index_title = 'Home'
 # Register your models here.
 @admin.register(Sock)
 class SockAdmin(admin.ModelAdmin):
-    readonly_fields = ["features"]
+    readonly_fields = ["features",
+                       "image_render",
+                       ]
     date_hierarchy = 'created_at'
-    list_display = ('created_at', 'owner')
+    list_display = ('image_render', 'created_at', 'owner')
+    fields = ('image_render',)
+
+    def image_render(self, obj):
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.image.url,
+            width=100,
+            height=100,
+            ))
 
 
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
     readonly_fields = []
     date_hierarchy = 'created_at'
-    list_display = ('similarity', 'sock1', 'sock2')
+    list_display = ('image_render', 'similarity', 'sock1', 'sock2',)
+    fields = ('image_render',)
 
+    def image_render(self, obj):
+        return mark_safe('<img src="{url1}" width="{width}" height={height} />\n '
+                         '<img src="{url2}" width="{width}" height={height} />'
+                         .format(
+                            url1=obj.sock1.image.url,
+                            url2=obj.sock2.image.url,
+                            width=100,
+                            height=100,
+                          ))
